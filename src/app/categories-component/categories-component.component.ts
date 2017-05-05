3b56914b318f13dffbc101cce11fdce7bf2ed8b6
Paul Thorpe <paulsthorpe@gmail>
2017-05-05 18:43:40 -0400

    idk

--- src/app/categories-component/categories-component.component.ts
+++ src/app/categories-component/categories-component.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'categories-component',
@@ -7,9 +7,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CategoriesComponentComponent implements OnInit {
 
-  constructor() { }
+  selectedCategory;
+
+  constructor() { 
+
+  }
+
+  
 
   ngOnInit() {
+	  this.selectedCategory = [];
+  }
+
+  logCategory(category){
+	  console.log(category);
   }
 
   categories = [
